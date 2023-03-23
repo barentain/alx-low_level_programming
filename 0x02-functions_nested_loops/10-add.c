6ffedce1cc7d02dab9306e757291e8717e9bb1ea
valentine <valentinemchunu11@gmail.com>
2023-03-22 19:03:10 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds 2 numbers and prints the sum
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
